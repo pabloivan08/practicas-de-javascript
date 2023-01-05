304f322cb08ba2ec2b2bb87107003d4b76a82589
Pablo Avlalos <pablo@badjupiter.com>
2023-01-04 19:08:52 -0600

    version 2 de calculadora

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("QUe tranzita mundo")
+const title = document.querySelector('.title')
+const input1 = document.querySelector('#calc1')
+const input2 = document.querySelector('#calc2')
+const button = document.querySelector('.button')
+const result = document.querySelector('.result')
+
+const isNumber = (value) => !isNaN(+value) ?  +value : value
+
+const actionButton = () => {
+  const resultado = isNumber(input1.value) + isNumber(input2.value)
+
+  result.innerText = `${input1.value} + ${input2.value} = ${resultado}`
+}
+
+button.addEventListener('click', actionButton)
